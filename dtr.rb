5441cf4d1e8825f4f57ba4e6b673aaf8306b6b88
Ian D. Alderman <ialderman@cyclecomputing.com>
2013-03-09 10:56:50 -0600

    Fixed typo

--- dtr.rb
+++ dtr.rb
@@ -8,7 +8,7 @@ ctr = 0
 ARGF.each do |l|
   i = ARGF.lineno
   p.schedule do
-    puts "String job #{i} to run command \"#{l.chomp}\" by thread #{Thread.current[:id]}"
+    puts "Starting job #{i} to run command \"#{l.chomp}\" by thread #{Thread.current[:id]}"
     system(l)
     puts "Job #{i} finished by thread #{Thread.current[:id]}"
   end
